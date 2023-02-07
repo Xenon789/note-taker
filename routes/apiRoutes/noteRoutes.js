6a1b2b809df9859001d3d4d86941ceb16439974c
BuildTools <unconfigured@null.spigotmc.org>
2023-02-06 23:31:30 -0800

    functions aren't working at the moment. Need to check routing as well as functions.

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,27 +1,24 @@
 const router = require('express').Router();
-const { createNewNote, deleteNote } = require('../../lib/notes');
+const { createNote, delNote } = require('../../lib/notes');
 let { notesArray } = require('../../db/db.json');
 
-// notes are available at api/notes in JSON
 router.get('/notes', (req, res) => {
   let results = notesArray;
   res.json(results);
 });
 
 router.post('/notes', (req, res) => {
-  // set id based on what the next index of the array will be
   if(notesArray){
     req.body.id = notesArray.length.toString();
   } else {
-    req.body.id = 0
+    req.body.id = 0;
   }
-  res.json(createNewNote(req.body, notesArray));
+  res.json(createNote(req.body, notesArray));
 });
 
-// Route parameters :👌 
 router.delete('/notes/:id', async (req, res) => {
   const { id } = req.params
-  notesArray = await deleteNote(id, notesArray);
+  notesArray = await delNote(id, notesArray);
   res.json(notesArray);
 });
 
